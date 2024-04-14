270868cd9f2fe733aee7e91133d26cb0dccd0d62
Jongita <jurgitajonaityte@gmail.com>
2024-04-14 20:17:26 +0300

    error added

--- src/names.ts
+++ src/names.ts
@@ -6,10 +6,25 @@ const suzinotiKilme=document.getElementById("suzinoti")!;
 
 suzinotiKilme.onclick=()=>{
         fetch(`https://api.nationalize.io/?name=${nameDOM.value}`)
-        .then((response)=>{
+          .then((response) => {
+            if (response.status === 429) {
+                let e = new Error(`Viršytas užklausų skaičius!`)
+                htmlResult.innerHTML= e.message;
+                htmlResult.className = "alarm"
+                nameDOM.value = '';
+                throw e;
+            }
+            if (response.status === 422) {
+                let e = new Error(`Nera tokio vardo!`)
+                htmlResult.innerHTML= e.message;
+                htmlResult.className = "alarm";
+                nameDOM.value = '';
+                throw e;
+            }
             return response.json();
         })
         .then( (data:Name)=>{
+            nameDOM.value = '';
             let str="";
             data.country.forEach((n)=>{
                 str+=`${n.country_id} ${((n.probability)*100).toFixed(2)} % <br>`
